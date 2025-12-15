e9b6c2b7c0c351c6b812b71613a30cd1c4e3aa6b
Maxime <maxime@miloctav.fr>
2025-12-15 14:44:30 +0100

    Delete global search bar

--- app/layouts/dashboard-oe.vue
+++ app/layouts/dashboard-oe.vue
@@ -1,30 +1,35 @@
 <template>
   <UDashboardGroup>
-    <UDashboardSidebar collapsible resizable :ui="{ footer: 'lg:border-t lg:border-default' }">
+    <UDashboardSidebar
+      collapsible
+      resizable
+      :ui="{ footer: 'lg:border-t lg:border-default' }"
+    >
       <template #header="{ collapsed }">
         <div class="w-full flex justify-center items-center pt-2">
           <NuxtLink to="/">
-            <img v-if="!collapsed" class="max-w-[70px] h-auto cursor-pointer mx-auto"
+            <img
+              v-if="!collapsed"
+              class="max-w-[70px] h-auto cursor-pointer mx-auto"
               alt="PME+ Engagé"
-              src="~/assets/images/Logo-PMEplus.png">
-            <img v-else class="h-8 w-8 cursor-pointer object-contain mx-auto" alt="PME+"
-              src="~/assets/images/Logo-PMEplus.png">
+              src="~/assets/images/Logo-PMEplus.png"
+            />
+            <img
+              v-else
+              class="h-8 w-8 cursor-pointer object-contain mx-auto"
+              alt="PME+"
+              src="~/assets/images/Logo-PMEplus.png"
+            />
           </NuxtLink>
         </div>
       </template>
 
       <template #default="{ collapsed }">
-        <UButton :label="collapsed ? undefined : 'Rechercher...'" icon="i-lucide-search" color="neutral"
-          variant="outline" block :square="collapsed">
-          <template v-if="!collapsed" #trailing>
-            <div class="flex items-center gap-0.5 ms-auto">
-              <UKbd value="meta" variant="subtle" />
-              <UKbd value="K" variant="subtle" />
-            </div>
-          </template>
-        </UButton>
-
-        <UNavigationMenu :collapsed="collapsed" :items="items" orientation="vertical" />
+        <UNavigationMenu
+          :collapsed="collapsed"
+          :items="items"
+          orientation="vertical"
+        />
       </template>
 
       <template #footer="{ collapsed }">
